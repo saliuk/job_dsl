2a35f1b4e90b6de3b70ab1b3a16a2718dc9980dc
itLab <ivan.saliuk@itlab-studio.com>
2024-12-09 17:00:31 +0200

    up

--- jobs/JOB_FOLDER_1/SUB_FOLDER_1_2/job_4.groovy
+++ jobs/JOB_FOLDER_1/SUB_FOLDER_1_2/job_4.groovy
@@ -9,6 +9,6 @@ folder('JOB_FOLDER_1/SUB_FOLDER_1_2') {
 job('JOB_FOLDER_1/SUB_FOLDER_1_2/job_4') {
     description('This is job 4 inside SUB_FOLDER_1_2 of JOB_FOLDER_1. It runs script4.sh from the scripts folder.')
     steps {
-        shell('bash ${WORKSPACE}/scripts/script4.sh')
+        shell('bash ${WORKSPACE}/SeedJob/scripts/script4.sh')
     }
 }
--- jobs/job1.groovy
+++ jobs/job1.groovy
@@ -1,6 +1,6 @@
 job('job1') {
     description('This is Job 1 at the root level. It runs script1.sh from the scripts folder.')
     steps {
-        shell('bash ${WORKSPACE}/scripts/script1.sh')
+        shell('bash ${WORKSPACE}/SeedJob/scripts/script1.sh')
     }
 }
