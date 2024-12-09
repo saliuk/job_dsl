7fd74908d048b3155e33a6782d9aa5d6c6e53489
itLab <ivan.saliuk@itlab-studio.com>
2024-12-09 16:13:42 +0200

    up

--- jobs/JOB_FOLDER_1/SUB_JOB_FOLDER_1_2/job_4.groovy
+++ jobs/JOB_FOLDER_1/SUB_JOB_FOLDER_1_2/job_4.groovy
@@ -1,4 +1,4 @@
-job('JOB_FOLDER_1/JOB_FOLDER_1.2/job_4') {
+job('JOB_FOLDER_1/SUB_JOB_FOLDER_1_2/job_4') {
     description('This is job 4 in JOB_FOLDER_1.2.')
     steps {
         shell('echo "Running job 4 in JOB_FOLDER_1.2"')
